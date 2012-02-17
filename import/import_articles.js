23668341f7dae9f30514a1a8a89014ef3d1f7f6d
Pedro Teixeira <pedro.teixeira@gmail.com>
2012-02-17 10:56:35 +0000

    importing images

--- import/import_articles.js
+++ import/import_articles.js
@@ -22,13 +22,46 @@ function articleContent(article) {
   return article.body;
 }
 
+function generateMetadata(file) {
+  return 'a:6:{s:5:"width";s:3:"612";s:6:"height";s:3:"612";s:14:"hwstring_small";s:22:"height='96' width='96'";s:4:"file";s:46:"' + file + '";s:5:"sizes";a:5:{s:9:"thumbnail";a:3:{s:4:"file";s:46:"8f4cd690348711e19e4a12313813ffc0_7-150x150.jpg";s:5:"width";s:3:"150";s:6:"height";s:3:"150";}s:6:"medium";a:3:{s:4:"file";s:46:"8f4cd690348711e19e4a12313813ffc0_7-300x300.jpg";s:5:"width";s:3:"300";s:6:"height";s:3:"300";}s:14:"post-thumbnail";a:3:{s:4:"file";s:46:"8f4cd690348711e19e4a12313813ffc0_7-612x288.jpg";s:5:"width";s:3:"612";s:6:"height";s:3:"288";}s:13:"large-feature";a:3:{s:4:"file";s:46:"8f4cd690348711e19e4a12313813ffc0_7-612x288.jpg";s:5:"width";s:3:"612";s:6:"height";s:3:"288";}s:13:"small-feature";a:3:{s:4:"file";s:46:"8f4cd690348711e19e4a12313813ffc0_7-300x300.jpg";s:5:"width";s:3:"300";s:6:"height";s:3:"300";}}s:10:"image_meta";a:10:{s:8:"aperture";s:1:"0";s:6:"credit";s:0:"";s:6:"camera";s:0:"";s:7:"caption";s:0:"";s:17:"created_timestamp";s:1:"0";s:9:"copyright";s:0:"";s:12:"focal_length";s:1:"0";s:3:"iso";s:1:"0";s:13:"shutter_speed";s:1:"0";s:5:"title";s:0:"";}}'
+}
+
+function insertArticleImages(article, next) {
+  async.forEach(article.images, function(image, next) {
+    console.log('image:', image);
+    return;
+    wpdb.query([
+      "INSERT INTO wp_posts (post_author, post_date, post_date_gmt, post_content, post_title, post_excerpt, post_status, comment_status, ping_status, post_name, post_modified, post_modified_gmt, post_parent, post_type, comment_count)",
+      "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    ].join(' '), [DEFAULT_AUTHOR_ID, article.created, article.created, '', image , '', 'inherit', 'closed', 'open', image, article.modified, article.modified, article.wp_post_id, 'attachment', 0], function(err, res) {
+      if (err) { return next(err); }
+      wpdb.query([
+        "INSERT INTO wp_postmeta(post_id, meta_key, meta_value)",
+        "VALUES (?, ?, ?)"
+      ].join(' '), [article.wp_post_id, '_wp_attached_file', image], function(err) {
+        if (err) { return next(err); }
+        wpdb.query([
+          "INSERT INTO wp_postmeta(post_id, meta_key, meta_value)",
+          "VALUES (?, ?, ?)"
+        ].join(' '), [article.wp_post_id, '_wp_attachment_metadata', image], function(err) {
+        
+      });
+    });
+  });
+  
+}
+
 function insertArticle(article, done) {
+  console.log('going to insert article:', article);
   wpdb.query([
     "INSERT INTO wp_posts(post_author, post_date, post_date_gmt, post_content, post_title, post_excerpt, post_status, comment_status, ping_status, post_name, post_modified, post_modified_gmt, post_type, comment_count)",
     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
   ].join(' '), [DEFAULT_AUTHOR_ID, article.created, article.created, articleContent(article), article.title , article.intro, 'publish', 'closed', 'open', slug(article.title), article.modified, article.modified, 'post', 0], function(err, res) {
     if (err) { return done(err); }
     var postId = res.insertId;
+    
+    article.wp_post_id = postId;
+    
     
     if (! article.themes.length) { return done(); }
     
@@ -98,7 +131,6 @@ function insertArticle(article, done) {
                         .map(function(res) { return db.escape(iconv.convert(res.body).toString()); })
                         ;
 
-      console.error('categories:', categories);
       if (categories.length === 0) {
         return done(null, []);
       }
@@ -114,7 +146,6 @@ function insertArticle(article, done) {
         if (res.length < 1) {
           return done(new Error('Couldn\'t find a taxonomy for ' + inspect(categories)));
         }
-        console.error('categories for article %s: %j', article.title , res);
         done(null, res);
       });
     });
@@ -133,46 +164,38 @@ function insertArticle(article, done) {
   function getArticleImages(article, next) {
     
     function getMainImage(article, next) {
-
       if (! article.overview_image_file_id) { return next(); }
-
       db.query([
-        "SELECT *",
-        "FROM act2_media_files",
-        "WHERE id = ?"
-      ].join(' '), [article.overview_image_file_id], function(err, res) {
+        "SELECT act2_media_files.id, act2_media_files.type",
+        "FROM act2_gallery_images, act2_media_files",
+        "WHERE act2_gallery_images.id = ?",
+      ].join(' '), [article.image_gallery_id], function(err, file) {
         if (err) { return next(err); }
-        var extension = getExtension(res.type);
-        var filePath = '/file/' + res.id + '.' + extension;
-        fs.writeFile(__dirname + '/' + filePath, res.content, function(err) {
-          if (err) { return next(err); }
-          article.mainImage = filePath;
-          return next(null);
-        });
+        if (Array.isArray(file)) { file = file[0]; };
+        if (! file) { return next(); }
+        var extension = getExtension(file.type);
+        var filePath = file.id + '.' + extension;
+        article.mainImage = filePath;
+        return next(null);
       });
-      
     }
     
     function getGalleryImages(article, next) {
-      if (! article.article.image_gallery_id) { return next(); }
+      if (! article.image_gallery_id) { return next(); }
       db.query([
-        "SELECT act2_media_files.*",
+        "SELECT act2_media_files.id, act2_media_files.type",
         "FROM act2_gallery_images, act2_media_files",
-        "WHERE act2_gallery_images.id = ?",
-        "AND",
-        "act2_media_files.gallery_id = act2_gallery_images.image_file_id"
+        "WHERE act2_gallery_images.gallery_id = ?",
+        "AND act2_gallery_images.image_file_id = act2_media_files.id"
       ].join(' '), [article.image_gallery_id], function(err, res) {
         if (err) { return next(err); }
         if (! Array.isArray(res)) { res = [res]; }
         async.forEach(res, function(image, next) {
           var extension = getExtension(image.type);
-          var filePath = '/file/' + image.id + '.' + extension;
-          fs.writeFile(__dirname + '/' + filePath, image.content, function(err) {
-            if (err) { return next(err) }
-            if (! article.images) { article.images = []};
-            article.images.push(filePath);
-            next();
-          });
+          var filePath = image.id + '.' + extension;
+          if (! article.images) { article.images = []};
+          article.images.push(filePath);
+          next();
         }, next);
       });
     }
@@ -195,7 +218,6 @@ function insertArticle(article, done) {
         getTranslationsForArticle(article.id, function(err, props) {
           if (err) { return next(err); }
           for (prop in props) { article[prop] = props[prop]; }
-          console.error('going to get themes for article', article.title);
           getCategoriesForArticle(article, function(err, themes) {
             if (err) { return next(err); }
             article.themes = themes;
@@ -218,9 +240,14 @@ function insertArticle(article, done) {
 }(function(err, articles) {
   if (err) { throw err; }
   db.end();
-  console.log('done:', inspect(articles, false, null));
-  console.log('going to insert %d articles', articles.length);
   var inserted = 0;
+  insertArticle(articles[0], function(err) {
+    if (err) { throw err; }
+    console.log('done');
+  });
+  return;
+  
+  console.log('going to insert %d articles', articles.length);
   async.forEach(articles, function(article, next) {
     insertArticle(article, function(err) {
       if (err) { return next(err);}
--- import/import_images.js
+++ import/import_images.js
@@ -2,6 +2,10 @@ var mysql = require('mysql');
 var inspect = require('util').inspect;
 var async = require('async');
 var fs = require('fs');
+var gm = require('gm');
+var path = require('path');
+var Iconv = require('iconv').Iconv;
+var iconv = new Iconv('UTF-8', 'ISO-8859-1//TRANSLIT//IGNORE');
 
 var db = mysql.createClient({
   user: "root",
@@ -12,25 +16,61 @@ function getExtension(type) {
   switch(type) {
     case 'image/jpeg':
     case 'image/pjpeg':
-      return 'jpg';
+      return '.jpg';
     default:
-      return 'bin'
+      return '.bin'
   }
 }
 
+function convertFileName(fileName) {
+  return iconv.convert(fileName).toString().replace(/ /g, '_');
+}
+
+function canonicalFilePath(file) {
+  var fileName = convertFileName(file.name);
+  var extension = path.extname(fileName) || getExtension(file.type);
+  var filePathComponents = [__dirname, 'files', file.id + '-' + path.basename(fileName) + extension];
+  return filePathComponents.join('/');
+}
+
 function saveFile(file, next) {
-  fs.writeFile(__dirname + '/files/' + file.id + '.' + getExtension(file.type), file.content, next);
+  fs.writeFile(canonicalFilePath(file), file.content, next);
+}
+
+function generateAllSizes(file, next) {
+  function resize(width, height, next) {
+    var fileName = convertFileName(file.name);
+    var extension = path.extname(fileName) || getExtension(file.type);
+    var newFileName = file.id + '-' + path.basename(fileName) + '-' + width + 'x' + height + extension;
+    gm(canonicalFilePath(file))
+      .resize(width, height)
+      .write(__dirname + '/files/' + newFileName, next);
+  }
+  
+  async.parallel([
+    function(next) { resize(150, 150, next); },
+    function(next) { resize(300, 300, next); },
+    function(next) { resize(612, 288, next); },
+    function(next) { resize(612, 288, next); },
+  ], next);
+
 }
 
 (function getAllFiles() {
   db.query([
     "SELECT id FROM act2_media_files"
   ].join(' '), [], function(err, results) {
-    async.forEach(results, function(file, next) {
+    async.forEachLimit(results, 10, function(file, next) {
       db.query('SELECT * from act2_media_files WHERE id = ?', [file.id], function(err, file) {
         if (Array.isArray(file)) { file = file[0]; }
         console.log('file:', file.id);
-        saveFile(file, next);
+        saveFile(file, function(err) {
+          if (err) { return next(err); }
+          generateAllSizes(file, function(err) {
+            if (err) { console.log('error converting file', file, err); }
+            next();
+          });
+        });
       });
     }, function(err) {
       if (err) { throw err; }
